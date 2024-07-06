bd7517c4bc42751e14fadf68eb2de3bd2d048c00
Kori <kexin@korix.top>
2024-07-06 15:44:31 +0800

    feat: add canva type

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import { useSelf } from '@liveblocks/react'
+import { useCanRedo, useCanUndo, useHistory, useSelf } from '@liveblocks/react'
 import { Info } from './info'
 import { Participants } from './participants'
 import { Toolbar } from './toolbar'
+import { useState } from 'react'
+import { CanvasMode, CanvasState } from '@/types/canvas'
 
 type CanvasProps = {
   boardId: string
@@ -11,13 +13,29 @@ type CanvasProps = {
 export const Canvas = ({ boardId }: CanvasProps) => {
   const info = useSelf(me => me.info)
 
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  })
+
+  const history = useHistory()
+
+  const canUndo = useCanUndo()
+  const canRedo = useCanRedo()
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none ">
       <Info boardId={boardId} />
 
       <Participants />
 
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   )
 }
